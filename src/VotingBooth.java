552dad0cfae8530517ae9e4f239c16f9ec48e5eb
11000783 <josephaguilar2022@gmail.com>
2018-12-12 18:27:06 -0800

    Saved IP Voting booth

--- src/VotingBooth.java
+++ src/VotingBooth.java
@@ -1,7 +1,13 @@
 import javax.swing.JOptionPane;
 
 public class VotingBooth {
-public static void main(String[] args) {
-	String number=JOptionPane.showInputDialog(null, "How old are you?", "Voting Booth", JOptionPane.QUESTION_MESSAGE);
-}
+	public static void main(String[] args) {
+		String number = JOptionPane.showInputDialog(null, "How old are you?", "Voting Booth",
+				JOptionPane.QUESTION_MESSAGE);
+		int vote = Integer.parseInt(number);
+		if (vote > 18) {
+			JOptionPane.showMessageDialog(null, "Who would you like to be president?", "Voting Booth",
+					JOptionPane.QUESTION_MESSAGE);
+		}
+	}
 }
